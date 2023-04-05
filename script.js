79ae6d47f7d2a77dc7a064e383611f079f2c4995
Tefanny Young <tefannyyoung@Tefannys-MacBook-Pro.local>
2023-04-05 15:42:01 +1200

    first draft

---
+++ script.js
@@ -0,0 +1,254 @@
+function myAbout() {
+	var element = document.getElementById("about");
+	element.classList.remove("SectionHide");
+	element.classList.add("SectionShow");
+
+	var nav = document.getElementById("nav");
+	nav.classList.remove("SectionHide");
+	nav.classList.add("SectionShow");
+	
+	var link=document.getElementById("aboutlink");
+	link.classList.add("active");
+
+	var link=document.getElementById("resumelink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("certificationlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("projectlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("contactlink");
+	link.classList.remove("active");
+
+	var home = document.getElementById("home");
+	home.classList.remove("SectionShow");
+	home.classList.add("SectionHide");
+
+	var resume = document.getElementById("resume");
+	resume.classList.remove("SectionShow");
+	resume.classList.add("SectionHide");
+
+	var Certifications = document.getElementById("Certifications");
+	Certifications.classList.remove("SectionShow");
+	Certifications.classList.add("SectionHide");
+
+	var projects = document.getElementById("projects");
+	projects.classList.remove("SectionShow");
+	projects.classList.add("SectionHide");
+
+	var Contact = document.getElementById("Contact");
+	Contact.classList.remove("SectionShow");
+	Contact.classList.add("SectionHide");
+}
+
+function myHome() {
+	var element = document.getElementById("home");
+	element.classList.remove("SectionHide");
+	element.classList.add("SectionShow");
+
+	var nav = document.getElementById("nav");
+	nav.classList.remove("SectionShow");
+	nav.classList.add("SectionHide");
+
+	var home = document.getElementById("about");
+	home.classList.remove("SectionShow");
+	home.classList.add("SectionHide");
+
+	var resume = document.getElementById("resume");
+	resume.classList.remove("SectionShow");
+	resume.classList.add("SectionHide");
+
+	var Certifications = document.getElementById("Certifications");
+	Certifications.classList.remove("SectionShow");
+	Certifications.classList.add("SectionHide");
+
+	var projects = document.getElementById("projects");
+	projects.classList.remove("SectionShow");
+	projects.classList.add("SectionHide");
+
+	var Contact = document.getElementById("Contact");
+	Contact.classList.remove("SectionShow");
+	Contact.classList.add("SectionHide");
+}
+
+function myResume() {
+	var element = document.getElementById("resume");
+	element.classList.remove("SectionHide");
+	element.classList.add("SectionShow");
+
+	var nav = document.getElementById("nav");
+	nav.classList.remove("SectionHide");
+	nav.classList.add("SectionShow");
+
+	var link=document.getElementById("aboutlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("resumelink");
+	link.classList.add("active");
+
+	var link=document.getElementById("certificationlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("projectlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("contactlink");
+	link.classList.remove("active");
+
+	var home = document.getElementById("home");
+	home.classList.remove("SectionShow");
+	home.classList.add("SectionHide");
+
+	var about = document.getElementById("about");
+	about.classList.remove("SectionShow");
+	about.classList.add("SectionHide");
+
+	var Certifications = document.getElementById("Certifications");
+	Certifications.classList.remove("SectionShow");
+	Certifications.classList.add("SectionHide");
+
+	var projects = document.getElementById("projects");
+	projects.classList.remove("SectionShow");
+	projects.classList.add("SectionHide");
+
+	var Contact = document.getElementById("Contact");
+	Contact.classList.remove("SectionShow");
+	Contact.classList.add("SectionHide");
+}
+
+function myCertifications() {
+	var element = document.getElementById("Certifications");
+	element.classList.remove("SectionHide");
+	element.classList.add("SectionShow");
+
+	var nav = document.getElementById("nav");
+	nav.classList.remove("SectionHide");
+	nav.classList.add("SectionShow");
+
+	var link=document.getElementById("aboutlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("resumelink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("certificationlink");
+	link.classList.add("active");
+
+	var link=document.getElementById("projectlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("contactlink");
+	link.classList.remove("active");
+
+	var home = document.getElementById("home");
+	home.classList.remove("SectionShow");
+	home.classList.add("SectionHide");
+
+	var resume = document.getElementById("resume");
+	resume.classList.remove("SectionShow");
+	resume.classList.add("SectionHide");
+
+	var about = document.getElementById("about");
+	about.classList.remove("SectionShow");
+	about.classList.add("SectionHide");
+
+	var projects = document.getElementById("projects");
+	projects.classList.remove("SectionShow");
+	projects.classList.add("SectionHide");
+
+	var Contact = document.getElementById("Contact");
+	Contact.classList.remove("SectionShow");
+	Contact.classList.add("SectionHide");
+}
+
+function myProjects() {
+	var element = document.getElementById("projects");
+	element.classList.remove("SectionHide");
+	element.classList.add("SectionShow");
+
+	var nav = document.getElementById("nav");
+	nav.classList.remove("SectionHide");
+	nav.classList.add("SectionShow");
+
+	var link=document.getElementById("aboutlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("resumelink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("certificationlink");
+	link.classList.remove("active");
+
+	var link=document.getElementById("projectlink");
+	link.classList.add("active");
+
+	var link=document.getElementById("contactlink");
+	link.classList.remove("active");
+
+	var home = document.getElementById("home");
+	home.classList.remove("SectionShow");
+	home.classList.add("SectionHide");
+
+	var resume = document.getElementById("resume");
+	resume.classList.remove("SectionShow");
+	resume.classList.add("SectionHide");
+
+	var Certifications = document.getElementById("Certifications");
+	Certifications.classList.remove("SectionShow");
+	Certifications.classList.add("SectionHide");
+
+	var about = document.getElementById("about");
+	about.classList.remove("SectionShow");
+	about.classList.add("SectionHide");
+
+	var Contact = document.getElementById("Contact");
+	Contact.classList.remove("SectionShow");
+	Contact.classList.add("SectionHide");
+}
+
+function myContact() {
+		var element = document.getElementById("Contact");
+        element.classList.remove("SectionHide");
+        element.classList.add("SectionShow");
+
+        var nav = document.getElementById("nav");
+        nav.classList.remove("SectionHide");
+        nav.classList.add("SectionShow");
+
+		var link=document.getElementById("aboutlink");
+		link.classList.remove("active");
+	
+		var link=document.getElementById("resumelink");
+		link.classList.remove("active");
+	
+		var link=document.getElementById("certificationlink");
+		link.classList.remove("active");
+	
+		var link=document.getElementById("projectlink");
+		link.classList.remove("active");
+	
+		var link=document.getElementById("contactlink");
+		link.classList.add("active");
+
+        var home = document.getElementById("home");
+        home.classList.remove("SectionShow");
+        home.classList.add("SectionHide");
+
+        var resume = document.getElementById("resume");
+        resume.classList.remove("SectionShow");
+        resume.classList.add("SectionHide");
+
+        var Certifications = document.getElementById("Certifications");
+        Certifications.classList.remove("SectionShow");
+        Certifications.classList.add("SectionHide");
+
+        var projects = document.getElementById("projects");
+        projects.classList.remove("SectionShow");
+        projects.classList.add("SectionHide");
+
+        var about = document.getElementById("about");
+        about.classList.remove("SectionShow");
+        about.classList.add("SectionHide");
+}
